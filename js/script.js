1e2c4501e2e665672d7ed39f2270ea65e6fa324a
ambishalsaha01 <bishalsaha9989@gmail.com>
2022-02-28 12:42:05 -0800

    Load Phone Details

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
-// search phone
+// Search Phone Field
 const loadPhone = () => {
     const searchInput = document.getElementById('search-input')
     const searchInputValue = searchInput.value;
     searchInput.value = '';
-
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchInputValue}`
     fetch(url)
     .then(res => res.json())
     .then(data => displayPhone(data.data))
 }
 
-// display phone
+// Display Phone Field
 const displayPhone = phones => {
     // console.log(phones.data)
     for(const phone of phones){
@@ -24,7 +23,7 @@ const displayPhone = phones => {
             <div class="card-body">
                 <h5 class="card-title">Phone Name: ${phone.phone_name}</h5>
                 <p>Brand: ${phone.brand}</p>
-                <button onclick="PhoneDetail('${phone.slug}')">Explore</button>
+                <button class="btn btn-primary" onclick="PhoneDetails('${phone.slug}')">Explore</button>
             </div>
         </div>
         `
@@ -32,16 +31,17 @@ const displayPhone = phones => {
     }
 }
 
-// PhoneDetail
-const PhoneDetail = phone => {
+// Phone Details Field
+const PhoneDetails = phone => {
     const url = `https://openapi.programming-hero.com/api/phone/${phone}`;
     fetch(url)
     .then(res => res.json())
     .then(data => loadPhoneDetail(data))
 }
 
+// Load Phone Details Field
 const loadPhoneDetail = info => {
-    console.log(info)
+    // console.log(info)
     const phoneDetails = document.getElementById('phone-details');
     phoneDetails.innerHTML = `
     <div class="card w-75 my-5 mx-auto">
@@ -51,15 +51,23 @@ const loadPhoneDetail = info => {
             </div>
             <div class="col-md-8">
                 <div class="card-body">
-                    <h5 class="card-title">Name: ${info.data.name}</h5>
+                    <h3 class="card-title">Name: ${info.data.name}</h3>
                     <ul class="list-group list-group-flush">
                         <li class="list-group-item">Release Date: ${info.data.releaseDate}</li>
                         <li class="list-group-item">Brand: ${info.data.brand}</li>
                         <li class="list-group-item">ChipSet: ${info.data.mainFeatures.chipSet}</li>
                         <li class="list-group-item">Memory: ${info.data.mainFeatures.displaySize}</li>
                         <li class="list-group-item">Memory: ${info.data.mainFeatures.memory}</li>
-                        <li class="list-group-item">Brand: ${info.data.brand}</li>
-                        <li class="list-group-item">Brand: ${info.data.brand}</li>
+                        <li class="list-group-item">Storage: ${info.data.mainFeatures.storage}</li>
+                        <h3 class="mt-3">Sensor</h3>
+                        <li class="list-group-item">Sensor: ${info.data.mainFeatures.sensors}</li>
+                        <h3 class="mt-3">others</h3>
+                        <li class="list-group-item">Bluetooth: ${info.data.others.Bluetooth}</li>
+                        <li class="list-group-item">GPS: ${info.data.others.GPS}</li>
+                        <li class="list-group-item">NFC: ${info.data.others.NFC}</li>
+                        <li class="list-group-item">Radio: ${info.data.others.Radio}</li>
+                        <li class="list-group-item">USB: ${info.data.others.USB}</li>
+                        <li class="list-group-item">WLAN: ${info.data.others.WLAN}</li>
                     </ul>
                 </div>
             </div>
